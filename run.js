808baa66f6efcaa8e17e2cda22366969ec73fff7
Brett Schules <brettschules@gmail.com>
2018-05-07 19:06:49 -0400

    Can increase or decrease timer

--- run.js
+++ run.js
@@ -1,3 +1,56 @@
+ var timerElement;
+ var hours = 0;
+ var mintues = 0;
+
+
+function increase() {
+  getTime()
+
+  formatTime('+')
+
+}
+
+function decrease() {
+  getTime()
+
+  formatTime('-')
+}
+
+function getTime() {
+  var time = timerElement.innerText
+  for (var i = 0; i < time.length; i++) {
+    if (time[i] === ':') {
+      hours = time.slice(0, i);
+      mintues = time.slice(i + 1, time.length)
+    }
+  }
+}
+
+function formatTime(action) {
+  let intHours = parseInt(hours, 10)
+  let intMinutes = parseInt(mintues, 10)
+  let numOfMinutes = 0
+
+  for (var i = 0; i < intHours; i++) {
+    numOfMinutes += 60;
+  }
+
+  numOfMinutes += intMinutes
+  if (action === '+') {
+    numOfMinutes++
+  } else {
+    numOfMinutes--
+  }
+
+ var h = Math.floor(numOfMinutes / 60);
+ var m = numOfMinutes % 60;
+ h = h < 10 ? '0' + h : h;
+ m = m < 10 ? '0' + m : m;
+ timerElement.innerHTML = h + ':' + m;
+ console.log(timerElement)
+}
+
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -18,5 +71,7 @@ function startTimer(duration, display) {
 window.onload = function () {
     var fiveMinutes = 60 * 5,
         display = document.querySelector('#time');
+        timerElement = document.getElementById('set')
+
     startTimer(fiveMinutes, display);
 };
